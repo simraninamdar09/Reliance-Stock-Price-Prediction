0f2d1dac8b2356ed019d03d80839d281d84b3c30
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2023-07-04 14:29:39 +0530

    Update app(2).py

--- app(2).py
+++ app(2).py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
 from sklearn.preprocessing import MinMaxScaler
