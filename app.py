7280cd0659a12867b7706712d8fd7d4a90a94183
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2023-07-04 17:06:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,13 @@ st.title('Stock Market Price Forecasting')
 st.write('Predicting stock prices using an LSTM model')
 
 # Slider to select the number of days to forecast
-days = st.slider('Select the number of days to forecast', 1, 365, 1)
+#days = st.slider('Select the number of days to forecast', 1, 365, 1)
+# Create a calendar date picker
+days = st.date_input("Select a date")
 
+# Display the selected date
+if selected_date:
+    st.write("Selected date:", days)
 
 
 if st.button('Forecast'):
